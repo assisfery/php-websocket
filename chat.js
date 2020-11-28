eb756f9b7986d73d446934edeb3df204de414e24
Assis Ferreira <assisrunny@hotmail.com>
2020-11-28 15:53:19 -0100

    send and show messages

--- chat.js
+++ chat.js
@@ -5,12 +5,13 @@ $(document).ready(function(){
 });
 
 var clientWS = null;
+var username = null;
 
 function main()
 {
-	var u = $("#username").val();
+	username = $("#username").val();
 
-	if(!u)
+	if(!username)
 	{
 		swal("Username", "Insert a username!", "warning");
 		return;
@@ -26,7 +27,7 @@ function main()
 
 		clientWS.send(JSON.stringify({
 			action: "login",
-			content: u
+			content: username
 		}));
 	};
 
@@ -49,6 +50,15 @@ function main()
 		if(message.action == "login")
 		{
 			$("#message-box").append("<p>" + message.content + " connected.</p>");
+		}
+		else if(message.action == "message")
+		{
+			var c = "alert-secondary";
+
+			if(username == message.username)
+				c = "alert-info";
+
+			$("#message-box").append("<div class='alert " + c + "'><p class='m-0'><b>" + message.username + ": </b>" + message.content + "</p><div>");
 		}
 	};
 
--- server.php
+++ server.php
@@ -21,17 +21,36 @@ class MyChat implements MessageComponentInterface {
 
     public function onOpen(ConnectionInterface $conn) {
         $this->clients->attach($conn);
+
+        echo "\nNew Connection";
     }
 
     public function onMessage(ConnectionInterface $from, $msg) {
+
+        $message = json_decode($msg);
+
+        // when new user login/connect
+        // add its name to socket object
+        if($message->action == "login")
+        {
+            $from->username = $message->content;
+        }
+
+        $message->username = $from->username;
+
+        echo "\nMessage: " . json_encode($message);
+
         foreach ($this->clients as $client) {
             //if ($from != $client)
-               $client->send($msg);
+           //$client->send($msg);
+
+            $client->send(json_encode($message));
         }
     }
 
     public function onClose(ConnectionInterface $conn) {
         $this->clients->detach($conn);
+        echo "\nClose Connection";
     }
 
     public function onError(ConnectionInterface $conn, \Exception $e) {
